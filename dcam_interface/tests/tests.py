2df3119229614c1469dfdb1e527b337e2efe305c
Oscar Rosero <odroseroc@gmail.com>
2025-07-24 12:07:20 +0200

    Modified test.py from dcam_interface
    Included import paths to treat dcamimg and dcamlib as modules

--- dcam_interface/tests/tests.py
+++ dcam_interface/tests/tests.py
@@ -1,9 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 import ctypes as ct
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from dcamlib import *
-from dcamimg import *
+from dcam_interface.dcamlib import *
+from dcam_interface.dcamimg import *
 
 # Please note: when values are not parsed as reference, ctypes will 
 # convert to native python types. Thus, the variable to which we assign
