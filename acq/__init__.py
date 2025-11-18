58488353cc1d859a601ad8e5ca377649a0c0607e
Oscar Rosero <odroseroc@gmail.com>
2025-11-18 15:01:48 +0100

    Working on motor settings in gui

---
+++ acq/__init__.py
@@ -0,0 +1,4 @@
+from .acquisition import AcquisitionParams
+
+__all__ = ("AcquisitionParams",)
+
--- acq/acquisition.py
+++ acq/acquisition.py
@@ -5,19 +5,15 @@ import time
 from dataclasses import dataclass
 from pathlib import Path
 
-from recon import AcquisitionIndex
-from core.log_utils import no_op
-from xrsource import XRSController
-from motor import MotorController
-
 @dataclass
 class AcquisitionParams:
-    directory: str
-    revolutions: float
-    step: float
-    imgs_per_step = 5
+    step_deg: float
+    num_revs: int
+    imgs_per_step: int
+    start_pos_deg: float
+    base_folder: str
 
-def acq_sequence(xrsource: XRSController, motor: MotorController, cam: , log_fn=no_op):
+def acq_sequence(xrsource: XRSController, motor: MotorController, cam, log_fn=no_op):
     base_dir = Path(acq.directory)
     raw_dir = base_dir / 'raw'
     log_fn(f'Creating acquisition directory {raw_dir}')
